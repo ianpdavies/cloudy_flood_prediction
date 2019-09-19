1d11223a4c88e4965d16ae39ddfdfbc141723c25
yon.davies@gmail.com <yon.davies@gmail.com>
2019-09-18 19:47:43 -0700

    Added docstring to predict_with_uncertainty; fixed some matrix shape issues with function.

--- scripts/testing.py
+++ scripts/testing.py
@@ -16,45 +16,22 @@ from CPR.utils import preprocessing, timer
 # Parameters
 
 # Image to predict on
-# img_list = ['4115_LC08_021033_20131227_test']
-img_list = ['4101_LC08_027038_20131103_1',
-            '4101_LC08_027038_20131103_2',
-            '4101_LC08_027039_20131103_1',
-            '4115_LC08_021033_20131227_1',
-            '4337_LC08_026038_20160325_1']
+img_list = ['4115_LC08_021033_20131227_test']
+# img_list = ['4101_LC08_027038_20131103_1',
+#             '4101_LC08_027038_20131103_2',
+#             '4101_LC08_027039_20131103_1',
+#             '4115_LC08_021033_20131227_1',
+#             '4337_LC08_026038_20160325_1']
 
-pctls = [10, 20, 30, 40, 50, 60, 70, 80, 90]
-# pctls = [50]
-MC_PASSES = 100
+# pctls = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+pctls = [50]
+MC_PASSES = 5
 DROPOUT_RATE = 0.3
 
 # ==================================================================================
 # Functions
 
 def predict_with_uncertainty(model, X, MC_PASSES):
-    """
-    Runs a number of forward passes through the model as Monte Carlo simulations, then computes average prediction
-    and variance of predictions using dask.
-
-    Reqs: tensorflow.keras
-
-    Parameters
-    ----------
-    model : trained tensorflow.keras model
-    X : np.array
-        Input features
-    MC_PASSES : int
-        Number of Monte Carlo simulations to run
-
-    Returns
-    ----------
-    preds : list
-        Average prediction for each set of MC simulations (shape = (X,)
-    variances : list
-        Variance of MC predictions (shape = (X,)
-    mc_preds.h5 : h5 file
-        Binary file of all predictions from MC simulations (shape = (MC_PASSES, X))
-    """
     preds_path = data_path / 'predictions' / img / '{}'.format(img + '_clouds_' + str(pctl))
     bin_file = preds_path / 'mc_preds.h5'
     try:
@@ -63,30 +40,30 @@ def predict_with_uncertainty(model, X, MC_PASSES):
         pass
     # Initialize binary file to hold predictions
     with h5py.File(bin_file, 'w') as f:
-        mc_preds = f.create_dataset('mc_preds', shape=(1, X.shape[1]),
-                                    maxshape=(None, X.shape[1]),
+        mc_preds = f.create_dataset('mc_preds', shape=(X.shape[0], 1),
+                                    maxshape=(X.shape[0], None),
                                     chunks=True, compression='gzip')  # Create empty dataset with shape of data
-    for i in range(MC_PASSES):
-        if i % 10 == 0 or i == MC_PASSES - 1:
-            print('Running MC {}/{}'.format(i, MC_PASSES))
+    for k in range(MC_PASSES):
+        if k % 10 == 0 or k == MC_PASSES - 1:
+            print('Running MC {}/{}'.format(k, MC_PASSES))
         flood_prob = model.predict(X, batch_size=7000, use_multiprocessing=True)  # Predict
-        flood_prob = flood_prob[0]  # Drop probability of not flooded (0) to save space
+        flood_prob = flood_prob[:, 0]  # Drop probability of not flooded (0) to save space
         with h5py.File(bin_file, 'a') as f:
-            f['mc_preds'][-flood_prob.shape[0]:] = flood_prob  # Append preds to h5 file
-            if i < MC_PASSES:  # Resize to append next pass, if there is one
-                f['mc_preds'].resize((f['mc_preds'].shape[0] + flood_prob.shape[0]), axis=0)
+            f['mc_preds'][:, -1] = flood_prob  # Append preds to h5 file
+            if k < MC_PASSES-1:  # Resize to append next pass, if there is one
+                f['mc_preds'].resize((f['mc_preds'].shape[1] + 1), axis=1)
 
     # Calculate statistics
-    with h5py.File('mc_preds.h5', 'r') as f:
+    with h5py.File(bin_file, 'r') as f:
         dset = f['mc_preds']
         preds_da = da.from_array(dset)  # Open h5 file as dask array
-        means = preds_da.mean(axis=0)
+        means = preds_da.mean(axis=1)
         means = means.compute()
-        variances = preds_da.var(axis=0)
-        variances = variances.compute()
+        variance = preds_da.var(axis=1)
+        variance = variance.compute()
         preds = means.round()
 
-    return preds, variances
+    return preds, variance
 
 # ==================================================================================
 # Loop to do predictions
