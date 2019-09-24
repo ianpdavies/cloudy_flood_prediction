02ec5bb65d6ad6623d11d088f942b998b65e66ac
yon.davies@gmail.com <yon.davies@gmail.com>
2019-09-23 19:37:09 -0700

    Added NN without MCD

--- scripts/models.py
+++ scripts/models.py
@@ -4,10 +4,11 @@ import tensorflow.keras.backend as K
 
 # ---------------------------------------------------------------
 
-tf.keras.backend.clear_session()
+
 
 # Using Functional API
 # def get_NN_MCD():
+#     tf.keras.backend.clear_session()
 #     inputs = tf.keras.layers.Input(shape=(INPUT_DIMS))
 #     x = tf.keras.layers.Dropout(rate=DROPOUT_RATE)(inputs, training=True)
 #     x = tf.keras.layers.Dense(units=29, activation='relu')(x)
@@ -29,9 +30,10 @@ tf.keras.backend.clear_session()
 # ---------------------------------------------------------------
 # NN with dropout before ReLU layers 
 
-tf.keras.backend.clear_session()
 
-def get_nn_mcd1(INPUT_DIMS, DROPOUT_RATE):
+
+def get_nn_uncertainty1(INPUT_DIMS, DROPOUT_RATE):
+    tf.keras.backend.clear_session()
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Input(shape=INPUT_DIMS)),
     model.add(tf.keras.layers.Lambda(lambda x: K.dropout(x, level=DROPOUT_RATE))),
@@ -52,3 +54,22 @@ def get_nn_mcd1(INPUT_DIMS, DROPOUT_RATE):
     return model
 
 # ---------------------------------------------------------------
+
+def get_nn1(INPUT_DIMS):
+    tf.keras.backend.clear_session()
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Input(shape=INPUT_DIMS)),
+    model.add(tf.keras.layers.Dense(units=24,
+                                    activation='relu')),
+    model.add(tf.keras.layers.Dense(units=12,
+                                    activation='relu')),
+#     model.add(tf.keras.layers.Dropout(rate=dropout_rate)(training=True)),
+#     model.add(tf.keras.layers.Flatten()),
+    model.add(tf.keras.layers.Dense(2,
+                                    activation='softmax'))
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+#                   metrics=[tf.keras.metrics.Recall()])
+#                   metrics=[tf.keras.metrics.F1()])
+                  metrics=['sparse_categorical_accuracy'])
+    return model
