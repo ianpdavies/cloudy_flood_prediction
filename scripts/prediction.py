649bdbab20715f63283ff1940b79e5fc1ac396f2
yon.davies@gmail.com <yon.davies@gmail.com>
2019-10-25 16:37:50 -0700

    Added dynamic batch size + now allows for removal of deleted blank feature

--- scripts/prediction.py
+++ scripts/prediction.py
@@ -9,12 +9,14 @@ from tensorflow import keras
 import json
 # ==================================================================================
 
-def prediction(img_list, pctls, feat_list_new, data_path, batch, remove_perm):
+
+def prediction(img_list, pctls, feat_list_new, data_path, batch, remove_perm, **model_params):
     for j, img in enumerate(img_list):
         times = []
         preds_path = data_path / batch / 'predictions' / 'nn' / img
         bin_file = preds_path / 'predictions.h5'
         metrics_path = data_path / batch / 'metrics' / 'testing_nn' / img
+        batch_fraction = model_params["batch_size"]
 
         try:
             metrics_path.mkdir(parents=True)
@@ -23,16 +25,22 @@ def prediction(img_list, pctls, feat_list_new, data_path, batch, remove_perm):
 
         for i, pctl in enumerate(pctls):
             print('Preprocessing')
-            data_test, data_vector_test, data_ind_test = preprocessing(data_path, img, pctl, gaps=True)
+            data_test, data_vector_test, data_ind_test, feat_keep = preprocessing(data_path, img, pctl, gaps=True)
+            print('data vector test size:', data_vector_test.shape)
+            feat_list_keep = [feat_list_new[i] for i in feat_keep]  # Removed if feat was deleted in preprocessing
             if remove_perm:
-                perm_index = feat_list_new.index('GSW_perm')
-                flood_index = feat_list_new.index('flooded')
+                perm_index = feat_list_keep.index('GSW_perm')
+                flood_index = feat_list_keep.index('flooded')
                 data_vector_test[data_vector_test[:, perm_index] == 1, flood_index] = 0  # Remove flood water that is perm water
             data_vector_test = np.delete(data_vector_test, perm_index, axis=1)  # Remove GSW_perm column
             data_shape = data_vector_test.shape
             X_test, y_test = data_vector_test[:, 0:data_shape[1]-1], data_vector_test[:, data_shape[1]-1]
-            INPUT_DIMS = X_test.shape[1]
-
+            print('X_test shape', X_test.shape)
+            # Get batch size based on sample size
+            batch_size = np.ceil(np.count_nonzero(~np.isnan(y_test)) * batch_fraction).astype('int')
+            model_params['batch_size'] = batch_size
+            print('feat keep', feat_keep)
+            print('batch size', model_params['batch_size'])
             print('Predicting for {} at {}% cloud cover'.format(img, pctl))
 
             # There is a problem loading keras models: https://github.com/keras-team/keras/issues/10417
@@ -40,7 +48,8 @@ def prediction(img_list, pctls, feat_list_new, data_path, batch, remove_perm):
             start_time = time.time()
             model_path = data_path / batch / 'models' / 'nn' / img / '{}'.format(img + '_clouds_' + str(pctl) + '.h5')
             trained_model = tf.keras.models.load_model(model_path)
-            preds = trained_model.predict(X_test, batch_size=7000, use_multiprocessing=True)
+            print(model_params['batch_size'])
+            preds = trained_model.predict(X_test, batch_size=model_params['batch_size'], use_multiprocessing=True)
             preds = np.argmax(preds, axis=1)  # Display most probable value
 
             try:
@@ -55,7 +64,6 @@ def prediction(img_list, pctls, feat_list_new, data_path, batch, remove_perm):
                 f.create_dataset(str(pctl), data=preds)
 
             times.append(timer(start_time, time.time(), False))  # Elapsed time for MC simulations
-
 
         times = [float(i) for i in times]  # Convert time objects to float, otherwise valMetrics will be non-numeric
         times_df = pd.DataFrame(np.column_stack([pctls, times]),
