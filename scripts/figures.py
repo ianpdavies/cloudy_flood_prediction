5db4803b0fe52b5348a1fb650e65309690615a1a
yon.davies@gmail.com <yon.davies@gmail.com>
2019-11-01 16:36:44 -0700

    Instead of reprojecting tilted Landsat image for figure, just cropping out a section of an image.

--- scripts/figures.py
+++ scripts/figures.py
@@ -1,16 +1,27 @@
 from CPR.utils import preprocessing
 import matplotlib.pyplot as plt
 from CPR.configs import data_path
+import rasterio
+from rasterio.windows import Window
+import numpy as np
 
 # Displaying all feature layers of an image
-# Need to figure out how to reproject them to make them not tilted
+# Need to figure out how to reproject them to make them not tilted. Below is a solution where I just clip them instead,
+# but it would be nice to show more of the image
 img = '4444_LC08_044033_20170222_3'
 batch = 'v2'
 pctl = 50
 feat_list_new = ['GSW_maxExtent', 'GSW_distExtent', 'GSW_perm', 'aspect', 'curve', 'developed', 'elevation',
                  'forest', 'hand', 'other_landcover', 'planted', 'slope', 'spi', 'twi', 'wetlands', 'flooded']
 
-data_train, data_vector_train, data_ind_train, feat_keep = preprocessing(data_path, img, pctl, gaps=False)
+stack_path = data_path / 'images' / img / 'stack' / 'stack.tif'
+
+# Read only a portion of the image
+window = Window.from_slices((500, 1400), (270, 1300))
+with rasterio.open(stack_path, 'r') as src:
+    w = src.read(window=window)
+    w[w == -999999] = np.nan
+    w[np.isneginf(w)] = np.nan
 
 titles = feat_list_new
 fig, axes = plt.subplots(4, 4, figsize=(30, 20))
@@ -18,7 +29,7 @@ i = 0
 for j, col in enumerate(axes):
     for k, row in enumerate(axes):
         ax = axes[j][k]
-        ax.imshow(data_train[:, :, i])
+        ax.imshow(w[i])
         ax.set_title(titles[i], fontdict={'fontsize': 10})
         ax.axis('off')
         i += 1
