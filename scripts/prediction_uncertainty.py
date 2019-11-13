7d8cfaef463fff99a2b980140a37dd2c5d96c18f
Ian Davies <yon.davies@gmail.com>
2019-11-12 18:12:52 -0800

    prediction with MCD uncertainty updated to reflect most recent prediction function

--- scripts/prediction_uncertainty.py
+++ scripts/prediction_uncertainty.py
@@ -8,63 +8,109 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 import sys
 sys.path.append('../')
 from CPR.configs import data_path
-from models import get_nn_uncertainty1 as get_model
+import tensorflow as tf
 from CPR.utils import preprocessing, timer
-# ==================================================================================
-# Parameters
-
-# Image to predict on
-# img_list = ['4115_LC08_021033_20131227_test']
-# img_list = ['4101_LC08_027038_20131103_2']
-# already did '4101_LC08_027038_20131103_1', '4101_LC08_027038_20131103_2', '4101_LC08_027039_20131103_1'
-img_list = ['4115_LC08_021033_20131227_1', '4337_LC08_026038_20160325_1']
-
-pctls = [10, 20, 30, 40, 50, 60, 70, 80, 90]
-# pctls = [90]
-MC_PASSES = 100
-DROPOUT_RATE = 0.3
 
 # ==================================================================================
-# Functions
 
-def predict_with_uncertainty(model, X, MC_PASSES):
-    preds_path = data_path / 'predictions' / 'nn_mcd' / img / '{}'.format(img + '_clouds_' + str(pctl))
-    bin_file = preds_path / 'mc_preds.h5'
-    try:
-        preds_path.mkdir(parents=True)
-    except FileExistsError:
-        pass
-    # Initialize binary file to hold predictions
-    with h5py.File(bin_file, 'w') as f:
-        mc_preds = f.create_dataset('mc_preds', shape=(X.shape[0], 1),
-                                    maxshape=(X.shape[0], None),
-                                    chunks=True, compression='gzip')  # Create empty dataset with shape of data
-    for k in range(MC_PASSES):
-        if k % 10 == 0 or k == MC_PASSES - 1:
-            print('Running MC {}/{}'.format(k, MC_PASSES))
-        flood_prob = model.predict(X, batch_size=7000, use_multiprocessing=True)  # Predict
-        flood_prob = flood_prob[:, 0]  # Drop probability of not flooded (0) to save space
-        with h5py.File(bin_file, 'a') as f:
-            f['mc_preds'][:, -1] = flood_prob  # Append preds to h5 file
-            if k < MC_PASSES-1:  # Resize to append next pass, if there is one
-                f['mc_preds'].resize((f['mc_preds'].shape[1] + 1), axis=1)
 
-    print('Calculating statistics on MC predictions')
+def prediction_with_uncertainty(img_list, pctls, feat_list_new, data_path, batch, MC_PASSES, remove_perm, **model_params):
+    for j, img in enumerate(img_list):
+        times = []
+        accuracy, precision, recall, f1 = [], [], [], []
+        preds_path = data_path / batch / 'predictions' / 'nn' / img
+        vars_path = data_path / batch / 'variances' / 'nn' / img
+        mc_bin_file = preds_path / 'mc_preds.h5'
+        preds_bin_file = preds_path / 'predictions.h5'
+        vars_bin_file = vars_path / 'variances.h5'
+        metrics_path = data_path / batch / 'metrics' / 'testing_nn' / img
 
-    # Calculate statistics
-    with h5py.File(bin_file, 'r') as f:
-        dset = f['mc_preds']
-        preds_da = da.from_array(dset, chunks="400 MiB")  # Open h5 file as dask array
-        means = preds_da.mean(axis=1)
-        means = means.compute()
-        variance = preds_da.var(axis=1)
-        variance = variance.compute()
-        preds = means.round()
-        del(f)
+        try:
+            metrics_path.mkdir(parents=True)
+        except FileExistsError:
+            print('Metrics directory already exists')
+        try:
+            preds_path.mkdir(parents=True)
+        except FileExistsError:
+            print('Metrics directory already exists')
+        try:
+            vars_path.mkdir(parents=True)
+        except FileExistsError:
+            print('Metrics directory already exists')
 
-    os.remove(bin_file) # Delete predictions to save space on disk
+        for i, pctl in enumerate(pctls):
+            print('Preprocessing', img, pctl, '% cloud cover')
+            data_test, data_vector_test, data_ind_test, feat_keep = preprocessing(data_path, img, pctl, gaps=True)
+            feat_list_keep = [feat_list_new[i] for i in feat_keep]  # Removed if feat was deleted in preprocessing
+            if remove_perm:
+                perm_index = feat_list_keep.index('GSW_perm')
+                flood_index = feat_list_keep.index('flooded')
+                data_vector_test[data_vector_test[:, perm_index] == 1, flood_index] = 0  # Remove flood water that is perm water
+            data_vector_test = np.delete(data_vector_test, perm_index, axis=1)  # Remove GSW_perm column
+            data_shape = data_vector_test.shape
+            X_test, y_test = data_vector_test[:, 0:data_shape[1]-1], data_vector_test[:, data_shape[1]-1]
 
-    return preds, variance
+            # Initialize binary file to hold predictions
+            with h5py.File(mc_bin_file, 'w') as f:
+                f.create_dataset('mc_preds', shape=(X_test.shape[0], 1),
+                                 maxshape=(X_test.shape[0], None),
+                                 chunks=True, compression='gzip')  # Create empty dataset with shape of data
 
-# ==================================================================================
-# Loop to do predictions
+            start_time = time.time()
+            model_path = data_path / batch / 'models' / 'nn' / img / '{}'.format(img + '_clouds_' + str(pctl) + '.h5')
+            trained_model = tf.keras.models.load_model(model_path)
+
+            for k in range(MC_PASSES):
+                if k % 10 == 0 or k == MC_PASSES - 1:
+                    print('Running MC {}/{} for {} at {}% cloud cover'.format(k, MC_PASSES, img, pctl))
+                flood_prob = trained_model.predict(X_test, batch_size=model_params['batch_size'], use_multiprocessing=True)  # Predict
+                flood_prob = flood_prob[:, 0]  # Drop probability of not flooded (0) to save space
+                with h5py.File(mc_bin_file, 'a') as f:
+                    f['mc_preds'][:, -1] = flood_prob  # Append preds to h5 file
+                    if k < MC_PASSES - 1:  # Resize to append next pass, if there is one
+                        f['mc_preds'].resize((f['mc_preds'].shape[1] + 1), axis=1)
+                del flood_prob
+
+            # Calculate MC statistics
+            print('Calculating MC statistics for {} at {}% cloud cover'.format(img, pctl))
+            with h5py.File(mc_bin_file, 'r') as f:
+                dset = f['mc_preds']
+                preds_da = da.from_array(dset, chunks="400 MiB")  # Open h5 file as dask array
+                means = preds_da.mean(axis=1)
+                means = means.compute()
+                variance = preds_da.var(axis=1)
+                variance = variance.compute()
+                preds = means.round()
+                del f, means, preds_da, dset
+
+            os.remove(mc_bin_file)  # Delete predictions to save space on disk
+
+            print('Saving mean preds/vars for {} at {}% cloud cover'.format(img, pctl))
+            with h5py.File(preds_bin_file, 'a') as f:
+                if str(pctl) in f:
+                    print('Deleting earlier mean predictions')
+                    del f[str(pctl)]
+                f.create_dataset(str(pctl), data=preds)
+            with h5py.File(vars_bin_file, 'a') as f:
+                if str(pctl) in f:
+                    print('Deleting earlier variances')
+                    del f[str(pctl)]
+                f.create_dataset(str(pctl), data=variance)
+
+            times.append(timer(start_time, time.time(), False))  # Elapsed time for MC simulations
+
+            print('Evaluating predictions for {} at {}% cloud cover'.format(img, pctl))
+            accuracy.append(accuracy_score(y_test, preds))
+            precision.append(precision_score(y_test, preds))
+            recall.append(recall_score(y_test, preds))
+            f1.append(f1_score(y_test, preds))
+
+            del preds, X_test, y_test, trained_model, data_test, data_vector_test, data_ind_test
+
+        metrics = pd.DataFrame(np.column_stack([pctls, accuracy, precision, recall, f1]),
+                               columns=['cloud_cover', 'accuracy', 'precision', 'recall', 'f1'])
+        metrics.to_csv(metrics_path / 'metrics.csv', index=False)
+        times = [float(i) for i in times]  # Convert time objects to float, otherwise valMetrics will be non-numeric
+        times_df = pd.DataFrame(np.column_stack([pctls, times]),
+                                columns=['cloud_cover', 'testing_time'])
+        times_df.to_csv(metrics_path / 'testing_times.csv', index=False)
