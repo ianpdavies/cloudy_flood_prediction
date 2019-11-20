bc46fdaa097379607a616db8c5f38675acf4d227
ianpdavies <yon.davies@gmail.com>
2019-11-20 09:59:19 -0800

    configs portable to other computers

--- CPR/configs.py
+++ CPR/configs.py
@@ -2,6 +2,6 @@ from pathlib import Path
 import pathlib
 
 # Working directory should be the CPR repository
-project_path = Path(__file__).parent
+project_path = Path(__file__).parents[1]
 
 data_path = project_path / 'data'
