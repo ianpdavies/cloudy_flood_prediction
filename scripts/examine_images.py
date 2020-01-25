cae9fa366649beff79b68405413d8d9ee27b2f17
ianpdavies <yon.davies@gmail.com>
2020-01-24 19:01:29 -0800

    Remove uncertainty plots for now, added flood + FP/FN + cloud border overlay images

--- scripts/examine_images.py
+++ scripts/examine_images.py
@@ -1,189 +1,131 @@
 # This script is for examining images, features, and predictions to visually identify patterns
-# Plot the uncertainties
-import rasterio
 import numpy as np
-import matplotlib.pyplot as plt
-import numpy.ma as ma
-from matplotlib.patches import Patch
-from matplotlib.colors import ListedColormap
 import sys
+import matplotlib.pyplot as plt
+import rasterio
+from PIL import Image
 sys.path.append('../')
 from CPR.configs import data_path
-import h5py
-from CPR.utils import preprocessing, tif_stacker
 
+batch = 'v30'
 
-img = '4469_LC08_015036_20170502_1'
-pctl = 40
-batch = 'v21'
-uncertainty = True
-feat_list_new = ['GSW_maxExtent', 'GSW_distExtent', 'GSW_perm', 'aspect', 'curve', 'developed', 'elevation',
-                 'forest', 'hand', 'other_landcover', 'planted', 'slope', 'spi', 'twi', 'wetlands', 'flooded']
+feat_list_new = ['GSW_maxExtent', 'GSW_distExtent', 'aspect', 'curve', 'developed', 'elevation', 'forest',
+                 'hand', 'other_landcover', 'planted', 'slope', 'spi', 'twi', 'wetlands', 'GSW_perm', 'flooded']
+
+img_list = ['4444_LC08_044033_20170222_2',
+            '4101_LC08_027038_20131103_1',
+            '4101_LC08_027038_20131103_2',
+            '4101_LC08_027039_20131103_1',
+            '4115_LC08_021033_20131227_1',
+            '4115_LC08_021033_20131227_2',
+            '4337_LC08_026038_20160325_1',
+            '4444_LC08_043034_20170303_1',
+            '4444_LC08_043035_20170303_1',
+            '4444_LC08_044032_20170222_1',
+            '4444_LC08_044033_20170222_1',
+            '4444_LC08_044033_20170222_3',
+            '4444_LC08_044033_20170222_4',
+            '4444_LC08_044034_20170222_1',
+            '4444_LC08_045032_20170301_1',
+            '4468_LC08_022035_20170503_1',
+            '4468_LC08_024036_20170501_1',
+            '4468_LC08_024036_20170501_2',
+            '4469_LC08_015035_20170502_1',
+            '4469_LC08_015036_20170502_1',
+            '4477_LC08_022033_20170519_1',
+            '4514_LC08_027033_20170826_1']
+
+pctls = [10, 30, 50, 70, 90]
 myDpi = 300
 plt.subplots_adjust(top=0.98, bottom=0.055, left=0.024, right=0.976, hspace=0.2, wspace=0.2)
 
-stack_path = data_path / 'images' / img / 'stack' / 'stack.tif'
-plot_path = data_path / batch / 'plots' / img
-vars_bin_file = data_path / batch / 'variances' / img / 'variances.h5'
-preds_bin_file = data_path / batch / 'predictions' / img / 'predictions.h5'
-stack_path = data_path / 'images' / img / 'stack' / 'stack.tif'
-
 # ======================================================================================================================
-# Get data
-data_train, data_vector_train, data_ind_train, feat_keep = preprocessing(data_path, img, pctl, feat_list_new, gaps=False)
-feat_list_keep = [feat_list_new[i] for i in feat_keep]  # Removed if feat was deleted in preprocessing
-perm_index = feat_list_keep.index('GSW_perm')
-flood_index = feat_list_keep.index('flooded')
-data_vector_train[
-    data_vector_train[:, perm_index] == 1, flood_index] = 0  # Remove flood water that is perm water
-data_vector_train = np.delete(data_vector_train, perm_index, axis=1)  # Remove perm water column
-shape = data_vector_train.shape
-X_train, y_train = data_vector_train[:, 0:shape[1] - 1], data_vector_train[:, shape[1] - 1]
+# Plot RGB + FP/FN + cloud borders
 
-# ======================================================================================================================
-# Get predictions
-print('Fetching prediction variances for', str(pctl) + '{}'.format('%'))
-# Read predictions
-with h5py.File(vars_bin_file, 'r') as f:
-    variances = f[str(pctl)]
-    variances = np.array(variances)  # Copy h5 dataset to array
+for img in img_list:
+    img_path = data_path / 'images' / img
+    stack_path = img_path / 'stack' / 'stack.tif'
+    plot_path = data_path / batch / 'plots' / img
 
-data_test, data_vector_test, data_ind_test, feat_keep = preprocessing(data_path, img, pctl, gaps=True,
-                                                                      normalize=False)
-var_img = np.zeros(shape)
-var_img[:] = np.nan
-rows, cols = zip(data_ind_test)
-var_img[rows, cols] = variances
+    with rasterio.open(str(stack_path), 'r') as ds:
+        data = ds.read()
+        data = data.transpose((1, -1, 0))  # Not sure why the rasterio.read output is originally (D, W, H)
+        data[data == -999999] = np.nan
+        data[np.isneginf(data)] = np.nan
 
-print('Fetching flood predictions for', str(pctl) + '{}'.format('%'))
-# Read predictions
-with h5py.File(preds_bin_file, 'r') as f:
-    predictions = f[str(pctl)]
-    predictions = np.array(predictions)  # Copy h5 dataset to array
+    for pctl in pctls:
+        # Get RGB image --------------------------------------
+        rgb_file = plot_path / '{}'.format('rgb_img' + '.png')
+        rgb_img = Image.open(rgb_file)
 
-prediction_img = np.zeros(shape)
-prediction_img[:] = np.nan
-rows, cols = zip(data_ind_test)
-prediction_img[rows, cols] = predictions
+        # Get FP/FN image --------------------------------------
+        comparison_img_file = plot_path / '{}'.format('false_map' + str(pctl) + '.png')
+        flood_overlay = Image.open(comparison_img_file)
 
+        # Create cloud border image --------------------------------------
+        clouds_dir = data_path / 'clouds'
+        clouds = np.load(clouds_dir / '{0}'.format(img + '_clouds.npy'))
+        clouds[np.isnan(data[:, :, 0])] = np.nan
+        cloudmask = np.less(clouds, np.nanpercentile(clouds, pctl), where=~np.isnan(clouds))
+        np.sum(cloudmask) / (cloudmask.shape[0] * cloudmask.shape[1])
+        data[cloudmask] = -999999
+        data[data == -999999] = np.nan
 
-# ======================================================================================================================
-# ----------------------------------------------------------
-# Uncertainty
-# Reshape variance values back into image band
-with rasterio.open(stack_path, 'r') as ds:
-    shape = ds.read(1).shape  # Shape of full original image
+        from scipy.ndimage import binary_dilation, binary_erosion
+        cloudmask_binary = cloudmask.astype('int')
+        cloudmask_border = binary_erosion(cloudmask_binary, iterations=2)
+        cloudmask_border = binary_dilation(cloudmask_binary, iterations=3)
+        cloudmask_border = (cloudmask_border - cloudmask_binary)
+        # Convert border to yellow image
+        border = np.zeros((cloudmask_border.shape[0], cloudmask_border.shape[1], 4), 'uint8')
+        border[:, :, 0] = cloudmask_border * 255
+        border[:, :, 1] = cloudmask_border * 255
+        border[:, :, 2] = cloudmask_border * 0
+        border[:, :, 3] = cloudmask_border * 255
+        # Make non-border pixels transparent
+        indices = np.where((border[:, :, 0] == 0) & (border[:, :, 1] == 0) & (border[:, :, 2] == 0) & (border[:, :, 3] == 0))
+        rows, cols = zip(indices)
+        border[rows, cols, 0] = 0
+        border[rows, cols, 1] = 0
+        border[rows, cols, 2] = 0
+        border[rows, cols, 3] = 0
+        border_rgb = Image.fromarray(border, mode='RGBA')
 
-aleatoric_img = np.zeros(shape)
-aleatoric_img[:] = np.nan
-rows, cols = zip(data_ind_test)
-aleatoric_img[rows, cols] = aleatoric_uncertainties
+        # Get flooded image (remove perm water) --------------------------------------
+        flood_index = feat_list_new.index('flooded')
+        perm_index = feat_list_new.index('GSW_perm')
+        indices = np.where((data[:, :, flood_index] == 1) & (data[:, :, perm_index] ==1))
+        rows, cols = zip(indices)
+        true_flood = data[:, :, flood_index]
+        true_flood[rows, cols] = 0
+        # Now convert to a gray color image
+        true_flood_rgb = np.zeros((true_flood.shape[0], true_flood.shape[1], 4), 'uint8')
+        true_flood_rgb[:, :, 0] = true_flood * 174
+        true_flood_rgb[:, :, 1] = true_flood * 236
+        true_flood_rgb[:, :, 2] = true_flood * 238
+        true_flood_rgb[:, :, 3] = true_flood * 255
+        # Make non-flood pixels transparent
+        indices = np.where((true_flood_rgb[:, :, 0] == 0) & (true_flood_rgb[:, :, 1] == 0) & (true_flood_rgb[:, :, 2] == 0) & (true_flood_rgb[:, :, 3] == 0))
+        rows, cols = zip(indices)
+        true_flood_rgb[rows, cols, 0] = 0
+        true_flood_rgb[rows, cols, 1] = 0
+        true_flood_rgb[rows, cols, 2] = 0
+        true_flood_rgb[rows, cols, 3] = 0
+        true_flood_rgb = Image.fromarray(true_flood_rgb, mode='RGBA')
 
-epistemic_img = np.zeros(shape)
-epistemic_img[:] = np.nan
-epistemic_img[rows, cols] = epistemic_uncertainties
+        # Plot all layers together --------------------------------------
+        # Convert black pixels to transparent in comparison image so it can overlay RGB
+        flood_datas = flood_overlay.getdata()
+        new_flood_datas = []
+        for item in flood_datas:
+            if item[0] == 0 and item[1] == 0 and item[2] == 0:
+                new_flood_datas.append((255, 255, 255, 0))
+            else:
+                new_flood_datas.append(item)
+        flood_overlay.putdata(new_flood_datas)
 
-predictions = np.array(np.argmax(aleatoric_softmax, axis=1))
-prediction_img = np.zeros(shape)
-prediction_img[:] = np.nan
-rows, cols = zip(data_ind_test)
-prediction_img[rows, cols] = predictions
-
-# ----------------------------------------------------------------
-# Plot trues and falses
-floods = data_test[:, :, 15]
-tp = np.logical_and(prediction_img == 1, floods == 1).astype('int')
-tn = np.logical_and(prediction_img == 0, floods == 0).astype('int')
-fp = np.logical_and(prediction_img == 1, floods == 0).astype('int')
-fn = np.logical_and(prediction_img == 0, floods == 1).astype('int')
-falses = fp + fn
-trues = tp + tn
-# Mask out clouds, etc.
-tp = ma.masked_array(tp, mask=p)
-tn = ma.masked_array(tn, mask=np.isnan(prediction_img))
-fp = ma.masked_array(fp, mask=np.isnan(prediction_img))
-fn = ma.masked_array(fn, mask=np.isnan(prediction_img))
-falses = ma.masked_array(falses, mask=np.isnan(prediction_img))
-trues = ma.masked_array(trues, mask=np.isnan(prediction_img))
-
-true_false = fp + (fn * 2) + (tp * 3)
-colors = ['saddlebrown',
-          'red',
-          'limegreen',
-          'blue']
-class_labels = ['True Negatives',
-                'False Floods',
-                'Missed Floods',
-                'True Floods']
-legend_patches = [Patch(color=icolor, label=label)
-                  for icolor, label in zip(colors, class_labels)]
-cmap = ListedColormap(colors)
-
-# fig, ax = plt.subplots(figsize=(10, 10))
-fig, ax = plt.subplots()
-ax.imshow(true_false, cmap=cmap)
-ax.legend(handles=legend_patches,
-          facecolor='white',
-          edgecolor='white')
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
-
-# myFig = ax.get_figure()
-# myFig.savefig(plot_path / 'truefalse.png', dpi=myDpi)
-# plt.close('all')
-
-# ----------------------------------------------------------------
-# Plot aleatoric uncertainty
-fig, ax = plt.subplots()
-fig_img = ax.imshow(aleatoric_img, cmap='plasma')
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
-im_ratio = aleatoric_img.shape[0] / aleatoric_img.shape[1]
-fig.colorbar(fig_img, ax=ax, fraction=0.046*im_ratio, pad=0.04*im_ratio)
-
-fig, ax = plt.subplots()
-fig_img = ax.imshow(epistemic_img, cmap='plasma')
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
-im_ratio = epistemic_img.shape[0] / epistemic_img.shape[1]
-fig.colorbar(fig_img, ax=ax, fraction=0.046*im_ratio, pad=0.04*im_ratio)
-
-# ----------------------------------------------------------------
-# Plot CIR image
-from CPR.utils import tif_stacker
-from PIL import Image, ImageEnhance
-
-print('Stacking image')
-band_list = ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7']
-tif_stacker(data_path, img, band_list, features=False, overwrite=False)
-spectra_stack_path = data_path / 'images' / img / 'stack' / 'spectra_stack.tif'
-
-
-# Function to normalize the grid values
-def normalize(array):
-    """Normalizes numpy arrays into scale 0.0 - 1.0"""
-    array_min, array_max = np.nanmin(array), np.nanmax(array)
-    return ((array - array_min) / (array_max - array_min))
-
-
-print('Processing CIR image')
-with rasterio.open(spectra_stack_path, 'r') as f:
-    nir, red, green = f.read(5), f.read(4), f.read(3)
-    nir[nir == -999999] = np.nan
-    red[red == -999999] = np.nan
-    green[green == -999999] = np.nan
-    nirn = normalize(nir)
-    redn = normalize(red)
-    greenn = normalize(green)
-    cir = np.dstack((nirn, redn, greenn))
-    cir[np.isnan(prediction_img)] = np.nan
-
-# Convert to PIL image, enhance, and save
-cir_img = Image.fromarray((cir * 255).astype(np.uint8()))
-cir_img = ImageEnhance.Contrast(cir_img).enhance(1.5)
-cir_img = ImageEnhance.Sharpness(cir_img).enhance(2)
-cir_img = ImageEnhance.Brightness(cir_img).enhance(2)
-
-fig, ax = plt.subplots()
-ax.imshow(cir_img)
-
+        # Superimpose comparison image and RGB image, then save and close
+        rgb_img.paste(true_flood_rgb, (0, 0), true_flood_rgb)
+        rgb_img.paste(flood_overlay, (0, 0), flood_overlay)
+        rgb_img.paste(border_rgb, (0, 0), border_rgb)
+        rgb_img.save(plot_path / '{}'.format('false_map_border' + str(pctl) + '.png'), dpi=(300, 300))
