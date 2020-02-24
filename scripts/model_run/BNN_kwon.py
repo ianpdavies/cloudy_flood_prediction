66d7ce275f46dc2986a9e8e23cb7ee16aea622b8
ianpdavies <yon.davies@gmail.com>
2020-02-23 17:13:44 -0800

    added better performing model

--- scripts/model_run/BNN_kwon.py
+++ scripts/model_run/BNN_kwon.py
@@ -1,6 +1,6 @@
 import __init__
 import tensorflow as tf
-from models import get_nn_bn1_kwon, get_nn_bn2_kwon, get_nn_bn2_kwon_v2
+from models import get_nn_bn2_kwon_v2 as model_func
 import os
 from training import training_bnn_kwon
 from prediction import prediction_bnn_kwon
@@ -17,22 +17,33 @@ print('Python Version:', sys.version)
 # Parameters
 
 pctls = [10, 30, 50, 70, 90]
+batch = 'BNN_kwon'
 batch_size = 8192
 epochs = 50
 MC_passes = 50
 dropout_rate = 0.2
 
+try:
+    (data_path / batch).mkdir()
+except FileExistsError:
+    pass
+
 # Get all images in image directory
 img_list = os.listdir(data_path / 'images')
 img_list.remove('4115_LC08_021033_20131227_test')
-pctls = [30]
-img_list = img_list[0:5]
+
 # Order in which features should be stacked to create stacked tif
 feat_list_new = ['GSW_maxExtent', 'GSW_distExtent', 'aspect', 'curve', 'developed', 'elevation', 'forest',
                  'hand', 'other_landcover', 'planted', 'slope', 'spi', 'twi', 'wetlands', 'GSW_perm', 'flooded']
 
 model_params = {'epochs': epochs,
                 'batch_size': batch_size}
+
+viz_params = {'img_list': img_list,
+              'pctls': pctls,
+              'data_path': data_path,
+              'batch': batch,
+              'feat_list_new': feat_list_new}
 
 NUM_PARALLEL_EXEC_UNITS = 8
 config = tf.compat.v1.ConfigProto(intra_op_parallelism_threads=NUM_PARALLEL_EXEC_UNITS, inter_op_parallelism_threads=4,
@@ -47,82 +58,13 @@ os.environ['GOTO_NUM_THREADS'] = str(NUM_PARALLEL_EXEC_UNITS)
 os.environ['OMP_NUM_THREADS'] = str(NUM_PARALLEL_EXEC_UNITS)
 
 # ======================================================================================================================
-# BNN with 1 layer
-model_func = get_nn_bn1_kwon
-batch = 'BNN_kwon1'
-viz_params = {'img_list': img_list,
-              'pctls': pctls,
-              'data_path': data_path,
-              'batch': batch,
-              'feat_list_new': feat_list_new}
-
-try:
-    (data_path / batch).mkdir()
-except FileExistsError:
-    pass
-
 training_bnn_kwon(img_list, pctls, model_func, feat_list_new, data_path, batch, dropout_rate, **model_params)
 prediction_bnn_kwon(img_list, pctls, feat_list_new, data_path, batch, MC_passes, **model_params)
 viz = VizFuncs(viz_params)
 viz.metric_plots()
-# viz.cir_image()
-# viz.time_plot()
-# viz.false_map(probs=False, save=False)
-# viz.false_map_borders()
 viz.metric_plots_multi()
-# viz.median_highlight()
-
-# ======================================================================================================================
-# BNN with 2 layer
-model_func = get_nn_bn2_kwon
-batch = 'BNN_kwon2'
-
-viz_params = {'img_list': img_list,
-              'pctls': pctls,
-              'data_path': data_path,
-              'batch': batch,
-              'feat_list_new': feat_list_new}
-
-try:
-    (data_path / batch).mkdir()
-except FileExistsError:
-    pass
-
-training_bnn_kwon(img_list, pctls, model_func, feat_list_new, data_path, batch, dropout_rate, **model_params)
-prediction_bnn_kwon(img_list, pctls, feat_list_new, data_path, batch, MC_passes, **model_params)
-viz = VizFuncs(viz_params)
-viz.metric_plots()
-# viz.cir_image()
-# viz.time_plot()
-# viz.false_map(probs=False, save=False)
-# viz.false_map_borders()
-viz.metric_plots_multi()
-# viz.median_highlight()
-
-# ======================================================================================================================
-# BNN with 2 layer, rearranging a bit
-batch = 'BNN_kwon2_v2'
-model_func = get_nn_bn2_kwon_v2
-
-viz_params = {'img_list': img_list,
-              'pctls': pctls,
-              'data_path': data_path,
-              'batch': batch,
-              'feat_list_new': feat_list_new}
-
-try:
-    (data_path / batch).mkdir()
-except FileExistsError:
-    pass
-
-training_bnn_kwon(img_list, pctls, model_func, feat_list_new, data_path, batch, dropout_rate, **model_params)
-prediction_bnn_kwon(img_list, pctls, feat_list_new, data_path, batch, MC_passes, **model_params)
-viz = VizFuncs(viz_params)
-viz.metric_plots()
-# viz.cir_image()
-# viz.time_plot()
-# viz.false_map(probs=False, save=False)
-# viz.false_map_borders()
-viz.metric_plots_multi()
-# viz.median_highlight()
-
+viz.time_plot()
+viz.false_map(probs=False, save=False)
+viz.false_map_borders()
+viz.fpfn_map()
+viz.uncertainty_map_NN()
