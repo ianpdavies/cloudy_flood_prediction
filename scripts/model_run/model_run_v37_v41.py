4d9bf75e8fd674ba340c25a4acaf2896ed201642
ianpdavies <yon.davies@gmail.com>
2020-02-23 17:10:07 -0800

    minor parameter change

--- scripts/model_run/model_run_v37_v41.py
+++ scripts/model_run/model_run_v37_v41.py
@@ -34,47 +34,14 @@ print('Python Version:', sys.version)
 # Parameters
 pctls = [10, 30, 50, 70, 90]
 
-# To get list of all folders (images) in directory
-# img_list = os.listdir(data_path / 'images')
-
-img_list = ['4444_LC08_044033_20170222_2',
-            '4101_LC08_027038_20131103_1',
-            '4101_LC08_027038_20131103_2',
-            '4101_LC08_027039_20131103_1',
-            '4115_LC08_021033_20131227_1',
-            '4115_LC08_021033_20131227_2',
-            '4337_LC08_026038_20160325_1',
-            '4444_LC08_043034_20170303_1',
-            '4444_LC08_043035_20170303_1',
-            '4444_LC08_044032_20170222_1',
-            '4444_LC08_044033_20170222_1',
-            '4444_LC08_044033_20170222_3',
-            '4444_LC08_044033_20170222_4',
-            '4444_LC08_044034_20170222_1',
-            '4444_LC08_045032_20170301_1',
-            '4468_LC08_022035_20170503_1',
-            '4468_LC08_024036_20170501_1',
-            '4468_LC08_024036_20170501_2',
-            '4469_LC08_015035_20170502_1',
-            '4469_LC08_015036_20170502_1',
-            '4477_LC08_022033_20170519_1',
-            '4514_LC08_027033_20170826_1']
+# Get all images in image directory
+img_list = os.listdir(data_path / 'images')
+img_list.remove('4115_LC08_021033_20131227_test')
 
 # Order in which features should be stacked to create stacked tif
 feat_list_new = ['GSW_maxExtent', 'GSW_distExtent', 'aspect', 'curve', 'developed', 'elevation',
                  'forest', 'hand', 'other_landcover', 'planted', 'slope', 'spi', 'twi', 'wetlands', 'GSW_perm',
                  'flooded']
-
-# Set some optimized config parameters
-NUM_PARALLEL_EXEC_UNITS = 4
-tf.config.threading.set_intra_op_parallelism_threads(NUM_PARALLEL_EXEC_UNITS)
-tf.config.threading.set_inter_op_parallelism_threads(2)
-tf.config.set_soft_device_placement(True)
-# tf.config.experimental.set_visible_devices(NUM_PARALLEL_EXEC_UNITS, 'CPU')
-os.environ["OMP_NUM_THREADS"] = str(NUM_PARALLEL_EXEC_UNITS)
-os.environ["KMP_BLOCKTIME"] = "30"
-os.environ["KMP_SETTINGS"] = "1"
-os.environ["KMP_AFFINITY"] = "granularity=fine,verbose,compact,1,0"
 
 
 # ======================================================================================================================
@@ -547,10 +514,10 @@ try:
 except FileExistsError:
     pass
 viz_params = {'img_list': img_list,
-'pctls': pctls,
-'data_path': data_path,
-'batch': batch,
-'feat_list_new': feat_list_new}
+              'pctls': pctls,
+              'data_path': data_path,
+              'batch': batch,
+              'feat_list_new': feat_list_new}
 log_reg_training(img_list, pctls, feat_list_new, data_path, batch, perm=None)
 prediction(img_list, pctls, feat_list_new, data_path, batch, perm=None, metric_perm=None)
 viz = VizFuncs(viz_params)
@@ -570,10 +537,10 @@ try:
 except FileExistsError:
     pass
 viz_params = {'img_list': img_list,
-'pctls': pctls,
-'data_path': data_path,
-'batch': batch,
-'feat_list_new': feat_list_new}
+              'pctls': pctls,
+              'data_path': data_path,
+              'batch': batch,
+              'feat_list_new': feat_list_new}
 log_reg_training(img_list, pctls, feat_list_new, data_path, batch, perm=0)
 prediction(img_list, pctls, feat_list_new, data_path, batch, perm=0, metric_perm=0)
 viz = VizFuncs(viz_params)
@@ -616,10 +583,10 @@ try:
 except FileExistsError:
     pass
 viz_params = {'img_list': img_list,
-'pctls': pctls,
-'data_path': data_path,
-'batch': batch,
-'feat_list_new': feat_list_new}
+              'pctls': pctls,
+              'data_path': data_path,
+              'batch': batch,
+              'feat_list_new': feat_list_new}
 log_reg_training(img_list, pctls, feat_list_new, data_path, batch, perm=0)
 prediction(img_list, pctls, feat_list_new, data_path, batch, perm=0, metric_perm='NaN')
 viz = VizFuncs(viz_params)
@@ -639,10 +606,10 @@ try:
 except FileExistsError:
     pass
 viz_params = {'img_list': img_list,
-'pctls': pctls,
-'data_path': data_path,
-'batch': batch,
-'feat_list_new': feat_list_new}
+              'pctls': pctls,
+              'data_path': data_path,
+              'batch': batch,
+              'feat_list_new': feat_list_new}
 log_reg_training(img_list, pctls, feat_list_new, data_path, batch, perm='NaN')
 prediction(img_list, pctls, feat_list_new, data_path, batch, perm='NaN', metric_perm='NaN')
 viz = VizFuncs(viz_params)
@@ -651,5 +618,3 @@ viz.cir_image()
 viz.time_plot()
 viz.false_map(perm='NaN')
 viz.metric_plots_multi()
-
-
