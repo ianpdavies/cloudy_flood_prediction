a5770b3df12c431b8ba53b80e02559d5616f4311
ianpdavies <yon.davies@gmail.com>
2020-07-22 12:01:21 -0700

    Soil layer now created from vector (feature class in ESRI geodatabase) instead of raster

--- scripts/soil.py
+++ scripts/soil.py
@@ -2,22 +2,25 @@ import __init__
 import numpy as np
 import rasterio
 import rasterio.plot
+from pathlib import Path
 from rasterio import features
 import matplotlib.pyplot as plt
 import geopandas as gpd
 from shapely.geometry import Polygon
 import os
 import sys
+import pandas as pd
+
 sys.path.append('../')
 from CPR.configs import data_path
 
+# os.environ['PROJ_LIB'] = 'C:/Users/ipdavies/.conda/pkgs/proj-6.2.1-h9f7ef89_0/Library/share/proj'
 # ======================================================================================================================
 # Define variables
 img_list = os.listdir(data_path / 'images')
 removed = {'4115_LC08_021033_20131227_test', '4444_LC08_044034_20170222_1',
            '4101_LC08_027038_20131103_2', '4594_LC08_022035_20180404_1', '4444_LC08_043035_20170303_1'}
 img_list = [x for x in img_list if x not in removed]
-
 
 soil_path = data_path / 'soil'
 
@@ -26,9 +29,9 @@ soil_path = data_path / 'soil'
 # satellite image dimensions
 
 for i, img in enumerate(img_list):
-    print('Image {}/{}, ({})'.format(i+1, len(img_list), img))
-# Grabbing a random tif file from zipped files. Can use stack.tif instead but will need to amend the code below
-    tif_file = 'zip://' + str(data_path / 'images' / img / img) + '.zip!' + img + '.aspect.tif'
+    print('Image {}/{}, ({})'.format(i + 1, len(img_list), img))
+    # Grabbing a random tif file from zipped files. Can use stack.tif instead but will need to amend the code below
+    tif_file = 'zip://' + str(data_path / 'images' / img / img) + '.zip!' + img + '_aspect.tif'
     with rasterio.open(tif_file, 'r', crs='EPSG:4326') as ds:
         img_bounds = ds.bounds
 
@@ -45,21 +48,35 @@ for i, img in enumerate(img_list):
 
     del counties
 
-    # Get soil maps and merge together
     soil_maps = []
     for state in states_select:
         file_name = 'gSSURGO_{}.gdb'.format(state)
-        soil_map = gpd.read_file(soil_path / file_name, layer='MUPOLYGON', crs='ESRI:102039')
+        mupolygon = gpd.read_file(soil_path / file_name, layer='MUPOLYGON', crs='ESRI:102039')
+        muaggatt = gpd.read_file(soil_path / file_name, layer='muaggatt')
+        muaggatt = pd.DataFrame(muaggatt)
+        muaggatt.drop(muaggatt.columns.difference(['mukey', 'hydgrpdcd']), 1, inplace=True)
+        # Map alpha hydro group codes to numeric
+        alpha_map = {'A': 1, 'A/D': 2, 'B': 3, 'B/D': 4, 'C': 5, 'C/D': 6, 'D': 7, '0': 0}
+        muaggatt['hydgrpdcd'] = muaggatt['hydgrpdcd'].fillna('0')
+        muaggatt = muaggatt.replace({'hydgrpdcd': alpha_map})
+        muaggatt.rename(columns={'mukey': 'MUKEY'}, inplace=True)
+        soil_map = mupolygon.merge(muaggatt, on='MUKEY')
         soil_map.crs = 'ESRI:102039'
         soil_map = soil_map.to_crs('EPSG:4326')
         soil_maps.append(soil_map)
     soil_merge = soil_maps[0]
     if len(soil_maps) > 1:
         soil_merge.append(soil_maps[1:])
-    soil_merge['MUSYM'] = soil_merge['MUSYM'].astype('int64')
+    soil_merge['MUKEY'] = soil_merge['MUKEY'].astype('int64')
+    soil_merge['hydgrpdcd'] = soil_merge['hydgrpdcd'].astype('int64')
     del soil_maps, soil_map
 
     # Burn soil map vector into raster
+
+    try:
+        Path(soil_path / 'rasters').mkdir(parents=True)
+    except FileExistsError:
+        pass
     out_file = soil_path / 'rasters' / '{}'.format(img + '_soil.tif')
     with rasterio.open(tif_file, 'r') as src:
         in_arr = src.read(1)
@@ -67,17 +84,19 @@ for i, img in enumerate(img_list):
         meta = src.meta.copy()
         meta.update(compress='lzw')
         with rasterio.open(out_file, 'w+', **meta) as out:
-            shapes = ((geom, value) for geom, value in zip(soil_merge.geometry, soil_merge.MUSYM))
+            shapes = ((geom, value) for geom, value in zip(soil_merge.geometry, soil_merge.hydgrpdcd))
             del soil_merge
             burned = features.rasterize(shapes=shapes, fill=np.nan, out=in_arr, transform=out.transform)
             out.write_band(1, burned)
+            del shapes
 
-# Examine images
-for i, img in enumerate(img_list):
-    print('Image {}/{}, ({})'.format(i + 1, len(img_list), img))
-    with rasterio.open(out_file, 'r', crs='EPSG:4326') as ds:
-        rasterio.plot.plotting_extent(ds)
-        fig, ax = plt.subplots(figsize=(8, 8))
-        rasterio.plot.show(ds, ax=ax, with_bounds=True)
-        plt.waitforbuttonpress()
-        plt.close()
+# # Examine images - requires clicking image to close window and continue with script
+# # Closing image window instead of clicking image will cause program to crash.
+# for i, img in enumerate(img_list):
+#     print('Image {}/{}, ({})'.format(i + 1, len(img_list), img))
+#     with rasterio.open(out_file, 'r', crs='EPSG:4326') as ds:
+#         rasterio.plot.plotting_extent(ds)
+#         fig, ax = plt.subplots(figsize=(8, 8))
+#         rasterio.plot.show(ds, ax=ax, with_bounds=True)
+#         plt.waitforbuttonpress()
+#         plt.close()
